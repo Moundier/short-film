87bebaf5fab83e61c594844369ea549412463a13
Moundier <garamoundier.developer@protonmail.com>
2024-02-28 22:23:59 -0300

    chore: resizable!

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostListener, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonCol, IonRow, IonIcon, IonButton, IonButtons, IonBackButton, IonMenuButton, IonRippleEffect, IonLabel, IonImg, IonAvatar, IonChip, IonText, IonPopover, IonList, IonItem } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { Data } from './data';
@@ -36,9 +36,34 @@ import { CommonModule } from '@angular/common';
     IonItem
   ],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+
+  @ViewChildren('dinamic') 
+  elements: QueryList<ElementRef> = new QueryList<ElementRef>();
 
   constructor() { }
+
+  ngOnInit(): void {
+    this.adjustElementHeight();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    console.log('Window resized:', event.target.innerWidth, 'x', event.target.innerHeight);
+    this.adjustElementHeight();
+  }
+
+  adjustElementHeight() {
+    const screenHeight = window.innerHeight;
+    const heightPercentage = 50; // Adjust this percentage as needed
+
+    for (const e of this.elements) {
+      const element = e.nativeElement;
+      console.log(e.nativeElement)
+      element.style.height = screenHeight * heightPercentage / 100 + 'px';
+    }
+  }
+  
 
   list: Data[] = [
     {
@@ -77,5 +102,7 @@ export class Tab1Page {
       disliked: 3,
       comments: 30
     },
-  ]
+  ];
+
+
 }
