9926a2ccebf73803785b21f9da954b36c2a14192
Moundier <garamoundier.developer@protonmail.com>
2024-03-02 21:11:48 -0300

    chore: slider!

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,24 +1,31 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonInput, IonToggle, IonLabel, IonNote, IonTextarea } from '@ionic/angular/standalone';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonInput, IonToggle, IonLabel, IonNote, IonTextarea, IonThumbnail } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader,
+  imports: [
+    IonHeader,
     IonToolbar,
     IonTitle,
     IonContent,
     ExploreContainerComponent,
     IonList,
     IonItem,
+    IonThumbnail,
     IonInput,
     IonToggle,
     IonLabel,
     IonNote,
-    IonTextarea],
+    IonTextarea
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class Tab3Page {
   constructor() {}
