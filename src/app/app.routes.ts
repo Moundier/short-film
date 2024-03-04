cf99a3adfa15ef2e3c064173e9981909d382d07a
Moundier <garamoundier.developer@protonmail.com>
2024-03-04 00:34:44 -0300

    chore: icons and images!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: '', /* set to be '' */
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },
+  {
+    path: 'login', /* set to be 'login' */
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./register/register.page').then( m => m.RegisterPage)
   },
 ];
