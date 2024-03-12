9b91b140169af70d426948ca730416d58d7ad377
Moundier <garamoundier.developer@protonmail.com>
2024-03-12 00:50:39 -0300

    fix: provider and importProvidergit add src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ApplicationInitStatus, enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
@@ -6,8 +6,8 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
-import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor, spinnerInterceptor, AuthInterceptor } from './app/app-state-manager/interceptor';
+import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor, } from './app/app-state-manager/interceptor';
 import { provideStore } from '@ngrx/store';
 
 if (environment.production) {
@@ -17,13 +17,12 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy, },
+    importProvidersFrom(HttpClientModule),
     provideIonicAngular(),
     provideRouter(routes),
-    provideStore(), // Info: ngrx store
-    importProvidersFrom(HttpClientModule), // Info: httpclient
+    provideStore(),
     provideHttpClient(
-      withInterceptors([authInterceptor, spinnerInterceptor]),
+      withInterceptors([authInterceptor]),
     ),
-    { provide : HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
 });
